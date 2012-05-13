8551249ee4fbcb6f4bbec40683844798cde9b679
Alexander Prokhorov <athari.p@gmail.com>
2012-05-14 02:40:51 +0400

    aggregate requires non-empty seq; added aggregateOrDefault, min, max, minBy, maxBy

--- YaLinqo/Enumerable.php
+++ YaLinqo/Enumerable.php
@@ -13,6 +13,8 @@ spl_autoload_register(function($class)
 
 class Enumerable implements \IteratorAggregate
 {
+    const ERROR_NO_ELEMENTS = 'Sequence contains no elements.';
+
     private $iterator;
 
     public function __construct ($iterator)
@@ -25,6 +27,7 @@ class Enumerable implements \IteratorAggregate
      */
     public function getIterator ()
     {
+        /** @var $it \Iterator */
         $it = call_user_func($this->iterator);
         $it->rewind();
         return $it;
@@ -115,26 +118,71 @@ class Enumerable implements \IteratorAggregate
     }
 
     /**
+     * <p>aggregate (func {{accum, value => result}) => result
+     * <p>aggregate (func {{accum, value, key => result}) => result
      * <p>aggregate (func {{accum, value => result}, seed) => result
      * <p>aggregate (func {{accum, value, key => result}, seed) => result
-     * @param Closure|array|string $func
-     * @param $seed mixed
+     * @param Closure|array|string $func {accum, value => result} | {accum, value, key => result}
+     * @param mixed $seed If seed is not null, the first element is used as seed. Default: null.
+     * @throws \InvalidOperationException If seed is null and sequence contains no elements.
+     * @return mixed
      */
     public function aggregate ($func, $seed = null)
     {
         $func = Utils::createLambda($func);
-        $result = $seed;
 
-        foreach ($this as $k => $v)
-            $result = call_user_func($func, $result, $v, $k);
+        $result = $seed;
+        if ($seed !== null) {
+            foreach ($this as $k => $v)
+                $result = call_user_func($func, $result, $v, $k);
+        }
+        else {
+            $assigned = false;
+            foreach ($this as $k => $v) {
+                if ($assigned)
+                    $result = call_user_func($func, $result, $v, $k);
+                else {
+                    $result = $v;
+                    $assigned = true;
+                }
+            }
+            if (!$assigned)
+                throw new \InvalidOperationException(self::ERROR_NO_ELEMENTS);
+        }
         return $result;
+    }
+
+    /**
+     * <p>aggregate (func {{accum, value => result}, default) => result
+     * <p>aggregate (func {{accum, value, key => result}, default) => result
+     * @param Closure|array|string $func {accum, value => result} | {accum, value, key => result}
+     * @param mixed $default Value to return if sequence is empty.
+     * @return mixed
+     */
+    public function aggregateOrDefault ($func, $default = null)
+    {
+        $func = Utils::createLambda($func);
+        $result = null;
+        $assigned = false;
+
+        foreach ($this as $k => $v) {
+            if ($assigned)
+                $result = call_user_func($func, $result, $v, $k);
+            else {
+                $result = $v;
+                $assigned = true;
+            }
+        }
+        return $assigned ? $result : $default;
     }
 
     /**
      * <p>average () => result
      * <p>average (selector {{value => result}) => result
      * <p>average (selector {{value, key => result}) => result
-     * @param Closure|array|string $selector
+     * @param Closure|array|string $selector {value => result} | {value, key => result}
+     * @throws \InvalidOperationException If sequence contains no elements.
+     * @return number
      */
     public function average ($selector = null)
     {
@@ -152,13 +200,14 @@ class Enumerable implements \IteratorAggregate
      * <p>count () => num
      * <p>count (selector {{value => result}) => num
      * <p>count (selector {{value, key => result}) => num
-     * @param Closure|array|string $selector
+     * @param Closure|array|string $selector {value => result} | {value, key => result}
+     * @return int
      */
     public function count ($selector = null)
     {
         $it = $this->getIterator();
 
-        if ($it instanceof \Countable)
+        if ($it instanceof \Countable && $selector === null)
             return count($it);
 
         $selector = Utils::createLambda($selector, Functions::$identity);
@@ -169,9 +218,83 @@ class Enumerable implements \IteratorAggregate
                 $count++;
         return $count;
     }
+
+    /**
+     * <p>max () => result
+     * <p>max (selector {{value => result}) => result
+     * <p>max (selector {{value, key => result}) => result
+     * @param Closure|array|string $selector {value => result} | {value, key => result}
+     * @throws \InvalidOperationException If sequence contains no elements.
+     * @return number
+     */
+    public function max ($selector = null)
+    {
+        $enum = $this;
+        if ($selector !== null)
+            $enum = $enum->select($selector);
+        return $enum->aggregate(function ($a, $b)
+        { return max($a, $b); });
+    }
+
+    /**
+     * <p>maxBy (comparer {{a, b => diff}) => result
+     * <p>maxBy (comparer {{a, b => diff}, selector {{value => result}) => result
+     * <p>maxBy (comparer {{a, b => diff}, selector {{value, key => result}) => result
+     * @param Closure|array|string $comparer {a, b => diff} Difference between a and b: &lt;0 if a&lt;b; 0 if a==b; &gt;0 if a&gt;b
+     * @param Closure|array|string $selector {value => result} | {value, key => result}
+     * @throws \InvalidOperationException If sequence contains no elements.
+     * @return number
+     */
+    public function maxBy ($comparer, $selector = null)
+    {
+        $comparer = Utils::createLambda($comparer, Functions::$compare);
+        $enum = $this;
+
+        if ($selector !== null)
+            $enum = $enum->select($selector);
+        return $enum->aggregate(function ($a, $b) use ($comparer)
+        { return call_user_func($comparer, $a, $b) > 0 ? $a : $b; });
+    }
+
+    /**
+     * <p>min () => result
+     * <p>min (selector {{value => result}) => result
+     * <p>min (selector {{value, key => result}) => result
+     * @param Closure|array|string $selector {value => result} | {value, key => result}
+     * @throws \InvalidOperationException If sequence contains no elements.
+     * @return number
+     */
+    public function min ($selector = null)
+    {
+        $enum = $this;
+        if ($selector !== null)
+            $enum = $enum->select($selector);
+        return $enum->aggregate(function($a, $b)
+        { return min($a, $b); });
+    }
+
+    /**
+     * <p>minBy (comparer {{a, b => diff}) => result
+     * <p>minBy (comparer {{a, b => diff}, selector {{value => result}) => result
+     * <p>minBy (comparer {{a, b => diff}, selector {{value, key => result}) => result
+     * @param Closure|array|string $comparer {a, b => diff} Difference between a and b: &lt;0 if a&lt;b; 0 if a==b; &gt;0 if a&gt;b
+     * @param Closure|array|string $selector {value => result} | {value, key => result}
+     * @throws \InvalidOperationException If sequence contains no elements.
+     * @return number
+     */
+    public function minBy ($comparer, $selector = null)
+    {
+        $comparer = Utils::createLambda($comparer, Functions::$compare);
+        $enum = $this;
+
+        if ($selector !== null)
+            $enum = $enum->select($selector);
+        return $enum->aggregate(function ($a, $b) use ($comparer)
+        { return call_user_func($comparer, $a, $b) > 0 ? $a : $b; });
+    }
 }
 
-$enum = Enumerable::from(array('a', 'b', 'c', 1, 'a' => 2, '10' => 3))
+$enum = Enumerable::from(array('a', 'bbb', 'c', 1, 'a' => 2, '10' => 3))
         ->where(
     function($v, $k)
     { return is_numeric($k); })
@@ -179,11 +302,18 @@ $enum = Enumerable::from(array('a', 'b', 'c', 1, 'a' => 2, '10' => 3))
     function($v, $k)
     { return "$v($k)"; });
 
+function compare_strlen ($a, $b)
+{
+    return strlen($a) - strlen($b);
+}
+
 foreach ($enum as $k => $v)
     echo "($k): ($v)\n";
 
 var_dump($enum->aggregate(function($a, $b)
 { return $a . '|' . $b; }, 'ooo'));
+var_dump($enum->aggregate(function($a, $b)
+{ return $a . '|' . $b; }));
 
 var_dump($enum->average(function($v, $k)
 { return $v + $k; }));
@@ -194,5 +324,26 @@ var_dump(Enumerable::from(new \EmptyIterator)->average());
 
 var_dump($enum->count(function($v)
 { return intval($v) != 0; }));
+var_dump(Enumerable::from(array(1, 2, 3))->count(function($v)
+{ return $v > 1; }));
 var_dump($enum->count());
 var_dump(Enumerable::from(new \EmptyIterator)->count());
+
+var_dump($enum->max(function($v, $k)
+{ return intval($k); }));
+var_dump(Enumerable::from(array(1, 2, 3))->max(function($v)
+{ return $v * $v; }));
+var_dump(Enumerable::from(array(1, 2, 3))->max());
+var_dump($enum->max());
+//var_dump(Enumerable::from(new \EmptyIterator)->max());
+
+var_dump($enum->min(function($v, $k)
+{ return intval($k); }));
+var_dump(Enumerable::from(array(1, 2, 3))->min(function($v)
+{ return $v * $v; }));
+var_dump(Enumerable::from(array(1, 2, 3))->min());
+var_dump($enum->min());
+//var_dump(Enumerable::from(new \EmptyIterator)->min());
+
+var_dump($enum->maxBy(__NAMESPACE__ . '\compare_strlen', function($v, $k)
+{ return $v . ' ' . $k; }));
--- YaLinqo/Functions.php
+++ YaLinqo/Functions.php
@@ -8,15 +8,33 @@ class Functions
     public static $identity;
     public static $true;
     public static $blank;
+    public static $compare;
 
     public static function init ()
     {
         self::$identity = function ($x)
-        { return $x; };
+        {
+            return $x;
+        };
+
         self::$true = function ()
-        { return true; };
+        {
+            return true;
+        };
+
         self::$blank = function ()
-        { };
+        {
+        };
+
+        self::$compare = function ($a, $b)
+        {
+            if ($a === $b)
+                return 0;
+            elseif ($a > $b)
+                return 1;
+            else
+                return -1;
+        };
     }
 }
 
