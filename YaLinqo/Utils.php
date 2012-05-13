9ae1b142fbf073ae3d0d05d9ac0e7629f8fc787d
Alexander Prokhorov <athari.p@gmail.com>
2012-05-13 22:51:40 +0400

    throw on createLamda from null with default null

--- YaLinqo/Utils.php
+++ YaLinqo/Utils.php
@@ -8,14 +8,19 @@ class Utils
     public static function createLambda ($closure, $default = null)
     {
         // TODO String lambda syntax: 'a => a*a'
-        if ($closure === null)
+        if ($closure === null) {
+            if ($default === null)
+                throw new \InvalidArgumentException;
             return $default; /*Functions::$identity*/
+        }
         if ($closure instanceof \Closure)
             return $closure;
         if (is_callable($closure))
             return $closure;
         /*return function() use($closure)
           { return call_user_func_array($closure, func_get_args()); };*/
+        if ($default === null)
+            throw new \InvalidArgumentException;
         return $default;
     }
 
