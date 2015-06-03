3a2ccb87895fa7df516b7a4c8e4f040c97a3cda6
Alexander Prokhorov <athari.p@gmail.com>
2015-06-04 02:08:08 +0300

    Added integration with Travis CI, Coveralls.

--- Tests/Testing/TestCase_Enumerable.php
+++ Tests/Testing/TestCase_Enumerable.php
@@ -7,28 +7,23 @@ use YaLinqo\Enumerable as E, YaLinqo\Functions;
 
 class TestCase_Enumerable extends \PHPUnit_Framework_TestCase
 {
-    static function setUpBeforeClass ()
-    {
-        \PHPUnit_Framework_ComparatorFactory::getDefaultInstance()->register(new \Tests\Testing\Comparator_ArrayEnumerable);
-    }
-
-    function setUp ()
+    protected function setUp ()
     {
         $this->setOutputCallback(function ($str) { return str_replace("\r\n", "\n", $str); });
     }
 
-    function assertEnumEquals (array $expected, E $actual, $maxLength = PHP_INT_MAX)
+    public static function assertEnumEquals (array $expected, E $actual, $maxLength = PHP_INT_MAX)
     {
-        $this->assertEquals($expected, $actual->take($maxLength));
+        self::assertEquals($expected, $actual->take($maxLength)->toArrayDeep());
     }
 
-    function assertEnumOrderEquals (array $expected, E $actual, $maxLength = PHP_INT_MAX)
+    public static function assertEnumOrderEquals (array $expected, E $actual, $maxLength = PHP_INT_MAX)
     {
-        $this->assertEquals($expected, $actual->take($maxLength)->select('array($k, $v)', Functions::increment()));
+        self::assertEquals($expected, $actual->take($maxLength)->select('array($k, $v)', Functions::increment())->toArrayDeep());
     }
 
-    function assertEnumValuesEquals (array $expected, E $actual, $maxLength = PHP_INT_MAX)
+    public static function assertEnumValuesEquals (array $expected, E $actual, $maxLength = PHP_INT_MAX)
     {
-        $this->assertEquals($expected, $actual->take($maxLength)->toValues());
+        self::assertEquals($expected, $actual->take($maxLength)->toValues()->toArrayDeep());
     }
 }
--- license.md
+++ license.md
@@ -1,17 +1,17 @@
-TL;DR: Simplified BSD License
+**Simplified BSD License**
 
-Copyright (c) 2012, Alexander Prokhorov
+Copyright © 2012–2014, Alexander Prokhorov
 All rights reserved.
 
 Redistribution and use in source and binary forms, with or without
 modification, are permitted provided that the following conditions are met:
 
-    * Redistributions of source code must retain the above copyright
-      notice, this list of conditions and the following disclaimer.
+* Redistributions of source code must retain the above copyright
+  notice, this list of conditions and the following disclaimer.
 
-    * Redistributions in binary form must reproduce the above copyright
-      notice, this list of conditions and the following disclaimer in the
-      documentation and/or other materials provided with the distribution.
+* Redistributions in binary form must reproduce the above copyright
+  notice, this list of conditions and the following disclaimer in the
+  documentation and/or other materials provided with the distribution.
 
 THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
 ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
