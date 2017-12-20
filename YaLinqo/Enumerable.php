155a7e7dd7d5a3718325450c04473ded416ad4cd
lucasjsoliveira <lucasjsoliveira@gmail.com>
2017-12-20 10:13:30 -0200

    Fix php 7.2 deprecated warning

--- YaLinqo/Enumerable.php
+++ YaLinqo/Enumerable.php
@@ -85,7 +85,7 @@ class Enumerable implements \IteratorAggregate
                 return $this->select(function ($v) { return (float)$v; });
             case 'null':
             case 'unset':
-                return $this->select(function ($v) { return (unset)$v; });
+                return $this->select(function ($v) { return null; });
             case 'object':
                 return $this->select(function ($v) { return (object)$v; });
             case 'string':
