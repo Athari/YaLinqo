a7373a81a7cdc1299a3ff859ea5bc5f8d048b464
Alexey Kopytko <alexey@kopytko.ru>
2018-03-17 23:25:38 +0900

    Test that F::init() indeed works.

--- Tests/Unit/FunctionsTest.php
+++ Tests/Unit/FunctionsTest.php
@@ -14,6 +14,7 @@ class FunctionsTest extends TestCaseEnumerable
     function testInit ()
     {
         F::init();
+        $this->assertNotEmpty(F::$identity);
     }
 
     function testIdentity ()
