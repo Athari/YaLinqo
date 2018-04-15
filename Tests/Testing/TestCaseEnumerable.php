8f7b419e99eb0081d91cf3528c18fa67166fb182
Alexander Prokhorov <athari.p@gmail.com>
2018-04-16 02:59:13 +0300

    Upgrade PHPUnit to 7.1.3.

--- Tests/Testing/TestCaseEnumerable.php
+++ Tests/Testing/TestCaseEnumerable.php
@@ -4,11 +4,18 @@ namespace YaLinqo\Tests\Testing;
 
 use YaLinqo\Enumerable as E, YaLinqo\Functions;
 
-class TestCaseEnumerable extends \PHPUnit_Framework_TestCase
+class TestCaseEnumerable extends \PHPUnit\Framework\TestCase
 {
     protected function setUp ()
     {
         $this->setOutputCallback(function ($str) { return str_replace("\r\n", "\n", $str); });
+    }
+
+    public function setExpectedException ($exception, $message = null)
+    {
+        $this->expectException($exception);
+        if ($message !== null)
+            $this->expectExceptionMessage($message);
     }
 
     public static function assertEnumEquals (array $expected, E $actual, $maxLength = PHP_INT_MAX)
