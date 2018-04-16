c63ced2919de62e79f998010ab452fe66c0b7f3e
Alexander Prokhorov <athari.p@gmail.com>
2018-04-16 08:27:42 +0300

    Updated docs: removed private members, forced menu to be always expanded.

--- phpdoc.cmd
+++ phpdoc.cmd
@@ -1,3 +1,4 @@
 set PATH=%PATH%;C:\Program Files (x86)\Graphviz\bin
 rmdir /s /q phpdocs
 php vendor/phpdocumentor/phpdocumentor/bin/phpdoc.php
+echo .collapse { height: auto !important; } >> "phpdocs\docs\css\template.css"
